f953b30de52484e9cc277df5ed7f9e6512f4d453
maki-ni <nehemiahelias14@gmail.com>
2025-12-10 23:01:50 +0300

    changed the dbFile.sql schema so that userid will be used instead of 'username'

--- backend/dbFile.sql
+++ backend/dbFile.sql
@@ -1,7 +1,8 @@
 USE lifeline_addis;
+
 CREATE TABLE User (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(10) unique,
+  user_id VARCHAR(10) unique,
   name VARCHAR(100),
   age INT,
   email VARCHAR(100) UNIQUE,
@@ -13,9 +14,9 @@ CREATE TABLE User (
 CREATE TABLE Patient (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name varchar(100),
-  username varchar(10) unique,
+  user_id varchar(10) unique,
   currentCondition VARCHAR(255),
-  FOREIGN KEY (username) REFERENCES User(username) ON DELETE CASCADE
+  FOREIGN KEY (user_id) REFERENCES User(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Staff (
@@ -23,7 +24,7 @@ CREATE TABLE Staff (
   username varchar(10) unique,
   profession VARCHAR(20),
   salary DECIMAL(10,2),
-  FOREIGN KEY (username) REFERENCES User(username) ON DELETE CASCADE 
+  FOREIGN KEY (user_id) REFERENCES User(user_id) ON DELETE CASCADE 
 );
 
 
--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,28 +1,28 @@
 const pool = require("../models/db");
 //fetches a specific user's full data
 exports.getUser = async (req, res) => {
-  const { username } = req.body;
+  const { user_id } = req.body;
   try {
-    const [rows] = await pool.query("SELECT * FROM User where username = ?", [
-      username,
+    const [rows] = await pool.query("SELECT * FROM User where user_id = ?", [
+      user_id,
     ]);
     res.send(rows[0]);
   } catch (err) {
-    console.err("Error fetching user", err);
+    console.error("Error fetching user", err);
   }
 };
 
-//for admin to create a new account
+//for a new person to create a new account
 exports.createUser = async (req, res) => {
-  const { name, username, password } = req.body;
+  const { name, user_id, password } = req.body;
   try {
     await pool.query(
-      `INSERT INTO User (username, name, password)
+      `INSERT INTO User (user_id, name, password)
         VALUES (?, ?, ?);`,
-      [username, name, password]
+      [user_id, name, password]
     );
   } catch (err) {
-    res.send("Error creating user", err);
+    res.send("Error creating user " + err.message);
   }
 };
 
