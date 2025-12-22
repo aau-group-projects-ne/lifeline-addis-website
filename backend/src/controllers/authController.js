cb7a2f7e7eeb2197756210355cde09f2fd9c0e51
maki-ni <nehemiahelias14@gmail.com>
2025-12-22 21:48:32 +0300

    changeed the erd diagram to match the most recent meeting

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,11 +1,27 @@
+const pool = require("../models/db");
 exports.login = (req, res) => {
-  const { username } = req.body;
+  const { username, password } = req.body;
   res.send(`Welcome ${username} `);
 };
 
-exports.register = (req, res) => {
-  
-  
+exports.register = async (req, res) => {
+  const { username, name, age, email, address, password, role } = req.body;
+  try {
+    await pool.query(
+      `INSERT INTO User (user_id, name, age, email, address, password)
+        VALUES (?, ?, ?, ?, ?, ?);`,
+      [username, name, age, email, address, password]
+    );
+    if (role == "patient") {
+      await pool.query(
+        `INSERT INTO PATIENT (name, user_id)
+        VALUES (?, ?);`,
+        [name, username]
+      );
+    }
+  } catch (err) {
+    res.send("Error creating user " + err.message);
+  }
 };
 
 exports.logout = (req, res) => {};
