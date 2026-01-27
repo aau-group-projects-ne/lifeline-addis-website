0d1df43aa2ff0aa8b7cc1d369d55aaf5145babb2
maki-ni <nehemiahelias14@gmail.com>
2026-01-28 01:37:12 +0300

    fixed image issues

--- Components/HomeNav.jsx
+++ Components/HomeNav.jsx
@@ -1,10 +1,11 @@
+import Image from "next/image";
 function HomeNav() {
   return (
     <>
       <header className="flex items-center justify-between border-b border-primary/10 bg-white/80 dark:bg-background-dark/80 backdrop-blur-md px-10 py-3 sticky top-0 z-50">
         <div className="flex items-center gap-4 text-primary">
           <div className="size-12">
-            <img
+            <Image
               className="w-full h-full"
               src="/tg_image_3199460643.jpeg"
               alt="Logo"
