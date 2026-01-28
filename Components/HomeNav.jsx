407e0f37aac6ec1c6ee30fcb236a2215a89adbd2
maki-ni <nehemiahelias14@gmail.com>
2026-01-28 06:57:28 +0300

    testing 1

--- Components/HomeNav.jsx
+++ Components/HomeNav.jsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 function HomeNav() {
   return (
     <>
-      <header className="max-w-[1200px] mx-auto flex items-center border-b border-primary/10 bg-white/80 dark:bg-background-dark/80 backdrop-blur-md px-6 py-2 sticky top-0 z-50">
-        <div className="flex items-center gap-4 text-primary">
+      <header className="w-full flex items-center justify-between border-b border-primary/10 bg-white/80 dark:bg-background-dark/80 backdrop-blur-md px-0 py-2 sticky top-0 z-50">
+        <div className="flex  items-center gap-4 text-primary">
           <div className="size-24">
             <Image
               width={192}
@@ -18,7 +18,7 @@ function HomeNav() {
           <h2 className="text-xl font-bold text-[#e63946]">HomeCare</h2>
         </div>
 
-        <nav className="ml-auto flex items-center gap-8 pr-6">
+        <nav className="ml-auto flex items-center justify-end gap-4 pr-0">
           {[
             ["Services", "services"],
             ["How It Works", "how-it-works"],
@@ -37,7 +37,7 @@ function HomeNav() {
           ))}
           <a
             href="/signup"
-            className="ml-4 px-5 py-3 rounded-xl bg-primary text-white text-base md:text-lg font-bold cursor-pointer hover:opacity-90 active:scale-[0.98]"
+            className="ml-2 px-5 py-3 rounded-xl bg-primary text-[#e63946] text-base md:text-l font-bold cursor-pointer hover:opacity-90 active:scale-[0.98]"
           >
             Get Care
           </a>
